4ec0a320f6661625710fb09c822602d2ace00d96
ugur ciftci <ugurprog@gmail.com>
2020-10-11 22:50:29 +0300

    create spring boot project

---
+++ bot/src/test/java/com/covid/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
