db96d58c7346af185e012a64282633a0a7c67d5a
ugur ciftci <ugurprog@gmail.com>
2020-10-12 18:41:34 +0300

    Update BotApplication.java

--- bot/src/main/java/com/covid/bot/BotApplication.java
+++ bot/src/main/java/com/covid/bot/BotApplication.java
@@ -7,6 +7,11 @@ import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 @SpringBootApplication
 public class BotApplication {
 
@@ -19,6 +24,33 @@ public class BotApplication {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void wakeUpConnection() {
+        try {
+            while (true) {
+
+                URL url = new URL("https://covid-status-bot.herokuapp.com/");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                int responcode = connection.getResponseCode();
+                System.out.println("RS CODE:" + responcode);
+                if (responcode == HttpURLConnection.HTTP_OK) {
+                    System.out.println("Working");
+                } else {
+                    System.out.println("GET not working");
+                }
+
+                Thread.sleep(60000); //
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
